7794efde586d8ff7d32651be49c458fd0154500d
Richwell111 <bcict20103@gmail.com>
2026-01-22 08:05:24 +0000

    added siggn in and sign up pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,15 +26,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+   
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+         <ClerkProvider>
+          <Navbar />
+          {children}
+          <Footer />
+          </ClerkProvider>
       </body>
     </html>
-    </ClerkProvider>
   );
 
 }
